2c44ad8e33d1f5d0d54812838c40e49bded1ba12
bipin11699-arch <bipin11699@gmail.com>
2026-09-18 17:15:28 +0530

    Update Firebase configuration with new project details

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,10 @@
+
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAoO-SZ2Gf8ZmVCmipJk_Nk_eqj0Jfvj8Q",
+  authDomain: "sttc-e9997.firebaseapp.com",
+  projectId: "sttc-e9997",
+  storageBucket: "sttc-e9997.firebasestorage.app",
+  messagingSenderId: "52519358254",
+  appId: "1:52519358254:web:e8caef95acd2b8f90f34bf",
+  measurementId: "G-X19EVTMSJP"
 };
